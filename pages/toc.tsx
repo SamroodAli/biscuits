20317672c1b09f8341a1bb4eea0787f1009d1766
samroodAli <samrood@superhire.net>
2022-11-03 21:27:30 +0530

    Style: table of contents page items

--- pages/toc.tsx
+++ pages/toc.tsx
@@ -43,16 +43,24 @@ export default function tableOfContents() {
       label: "Javascript",
       page: "/content/javascript",
     },
+    {
+      label: "Javascript",
+      page: "/content/javascript",
+    },
+    {
+      label: "Javascript",
+      page: "/content/javascript",
+    },
   ];
 
   return (
     <section className="border-2 border-gray-700 p-3 h-full">
-      <h2 className="h-5%">Table to contents</h2>
-      <ul className="mt-2 h-95% text-lg text-slate-900 grid md:grid-cols-4 overflow-y-auto">
+      <h2 className="h-5% text-4xl">Table to contents</h2>
+      <ul className="mt-2 h-95% w-6/12 mx-auto text-lg text-slate-900 overflow-y-auto pb-7">
         {contents.map((content) => (
           <li
             key={content.label}
-            className="h-14 md:h-[300px] m-3 border border-orange-900 text-gray-900 bg-orange-300  flex items-center justify-center p-4"
+            className="h-14 m-3 mt-4 p-4 border border-orange-900 text-gray-900 bg-orange-200 flex items-center justify-center hover:animate-bounce rounded-lg"
           >
             <Link href={content.label}>Javascript</Link>
           </li>
